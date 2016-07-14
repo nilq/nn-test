c81233f794eef0e9b81692f2ec20d0d6ca1fdc4d
nilq <i.am@nilq.dk>
2016-07-14 13:05:11 +0200

    Add randomseed and sigmoid function

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
 function love.load()
-
+  -- make randomness
+  math.randomseed(os.time())
+  -- put sigmoid function to math
+  math.sigmoid = function(z)
+    return 1 / (1 + math.exp(-z))
+  end
 end
 
 function love.update(dt)
